b236f6b29307c7eb83943e085501950940c20104
yulong.wyl <yulong.wyl@t430.lenovo>
2019-02-01 08:05:13 +0800

    Link vimrc

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+if [[ -d ~/.vim/bundle ]]
+then
+	echo "directory has exist"
+else
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+ln -sf `pwd`/.vimrc /home/yulong.wyl/.vimrc
